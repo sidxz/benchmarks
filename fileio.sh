668d0503580c8e3c9fe0cd1aa09c62251f234864
sidxz <sid@tamu.edu>
2017-05-18 11:40:20 -0500

    File IO

---
+++ fileio.sh
@@ -0,0 +1,6 @@
+sysbench --test=fileio --file-total-size=150G prepare
+
+sysbench --test=fileio --file-total-size=150G --file-test-mode=rndrw --init-rng=on --max-time=300 --max-requests=0 run
+
+sysbench --test=fileio --file-total-size=150G cleanup
+
