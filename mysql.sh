e912fa21109ed5ba99ea840ce17590bb0cd9d1a7
sidxz <sid@tamu.edu>
2017-05-22 23:52:48 -0500

    mysql

--- mysql.sh
+++ mysql.sh
@@ -1,3 +1,5 @@
+sysbench --test=oltp --oltp-table-size=100000 --mysql-db=test --mysql-user=root --mysql-password=BlackDog prepare
+
 echo "++++++++++++++++++++++8"
 sysbench --test=oltp --oltp-table-size=100000 --mysql-db=test --mysql-user=root --mysql-password=BlackDog --max-time=60 --oltp-read-only=on --max-requests=0 --num-threads=8 run
 
